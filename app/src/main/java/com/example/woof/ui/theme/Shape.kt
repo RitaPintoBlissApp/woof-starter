36b126e5a7b262cb3fcbeba0e32934f544bd9b29
RitaPintoBissApp <rpinto@blissapplications.com>
2023-11-15 20:24:22 +0000

    Change the Shape.kt

--- app/src/main/java/com/example/woof/ui/theme/Shape.kt
+++ app/src/main/java/com/example/woof/ui/theme/Shape.kt
@@ -20,6 +20,6 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(50.dp),
+    small = RoundedCornerShape(80.dp),
     medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp)
 )
